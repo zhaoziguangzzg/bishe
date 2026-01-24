bc978f3cda9bf36c96792572078df30d3776e34f
zzg <zzgzhaoziguang@qq.com>
2026-01-24 09:57:37 +0800

    information

--- internal/app/knowledge_sharing/model/information.go
+++ internal/app/knowledge_sharing/model/information.go
@@ -4,12 +4,15 @@ import "time"
 
 // information 定义消息结构体
 type Information struct {
-	Id             int        `json:"id" gorm:"column:id" mapstructure:"id"`
-	SendId         int        `json:"sendId" gorm:"column:send_id" mapstructure:"sendId"`
-	ReceiveAccount int        `json:"receiveAccount" gorm:"column:receive_account" mapstructure:"receiveAccount"`
-	Content        string     `json:"content" gorm:"column:content" mapstructure:"content"`
-	CreateAt       *time.Time `json:"createAt" gorm:"column:create_at" mapstructure:"-"`
-	CreateAtStr    string     `json:"-" gorm:"-" mapstructure:"createAt"`
+	Id int `json:"id" gorm:"column:id" mapstructure:"id"`
+	//发送uid
+	SendId int `json:"sendId" gorm:"column:send_id" mapstructure:"sendId"`
+	//接收uid
+	ReceiveAccount int `json:"receiveAccount" gorm:"column:receive_account" mapstructure:"receiveAccount"`
+	//消息内容
+	Content     string     `json:"content" gorm:"column:content" mapstructure:"content"`
+	CreateAt    *time.Time `json:"createAt" gorm:"column:create_at" mapstructure:"-"`
+	CreateAtStr string     `json:"-" gorm:"-" mapstructure:"createAt"`
 }
 
 // 指定Information对应的表名
--- internal/app/knowledge_sharing/service/information.go
+++ internal/app/knowledge_sharing/service/information.go
@@ -3,6 +3,7 @@ package service
 import (
 	"bishe/internal/app/knowledge_sharing/dao/mysql"
 	"bishe/internal/app/knowledge_sharing/model"
+	"time"
 )
 
 // 创建用户消息
@@ -13,4 +14,17 @@ func UserAddInformation(information *model.Information) (err error) {
 // 通知
 func AddUserNotice(notice *model.Information) (err error) {
 	return mysql.AddUserNotice(notice)
+}
+
+// 创建和发送消息
+func MakeAndSendNotice(sendId int, account int, content string, t time.Time) (err error) {
+	notice := &model.Information{
+		SendId:         sendId,
+		ReceiveAccount: account,
+		Content:        content,
+		CreateAt:       &t,
+	}
+
+	err = AddUserNotice(notice)
+	return
 }
