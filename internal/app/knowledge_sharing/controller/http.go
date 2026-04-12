8ebd03960bdbae399b05e3a71b4646b4692c290d
zzg <zzgzhaoziguang@qq.com>
2026-04-12 15:58:43 +0800

    添加用户http请求错误返回值

--- internal/app/knowledge_sharing/controller/http.go
+++ internal/app/knowledge_sharing/controller/http.go
@@ -12,15 +12,17 @@ const (
 	CODE_SYS_ERROR    int = 1
 	CODE_PARAMS_ERROR int = 1001
 
-	CODE_USER_NAME_EXIST       int = 2001
-	CODE_USER_NAME_NOT_EXIST   int = 2002
-	CODE_USER_PASSWORD_INVALID int = 2003
-	CODE_USER_NOT_LOGIN        int = 2004
-	CODE_USER_NAME_LEN_INVALID int = 2005
-	CODE_USER_EMAIL_INVALID    int = 2006
-	CODE_USER_PHONE_INVALID    int = 2007
-	CODE_USER_REPLACE          int = 2008
-	CODE_USER_AGE_INVALID      int = 2009
+	CODE_USER_NAME_EXIST              int = 2001
+	CODE_USER_NAME_NOT_EXIST          int = 2002
+	CODE_USER_PASSWORD_INVALID        int = 2003
+	CODE_USER_NOT_LOGIN               int = 2004
+	CODE_USER_NAME_LEN_INVALID        int = 2005
+	CODE_USER_EMAIL_INVALID           int = 2006
+	CODE_USER_PHONE_INVALID           int = 2007
+	CODE_USER_REPLACE                 int = 2008
+	CODE_USER_AGE_INVALID             int = 2009
+	CODE_PASSWORD_WRONG               int = 2010
+	CODE_USER_AVATAR_NAME_LEN_INVALID int = 2011
 
 	CODE_CHECKIN_REPEAT   int = 3001
 	CODE_COMMENT_TOO_LONG int = 3002
@@ -85,15 +87,17 @@ var CodeMsgMap map[int]string = map[int]string{
 	CODE_SYS_ERROR:    "服务出错，稍后再试",
 	CODE_PARAMS_ERROR: "参数错误",
 
-	CODE_USER_NAME_EXIST:       "用户名已存在",
-	CODE_USER_NAME_NOT_EXIST:   "用户名不存在",
-	CODE_USER_PASSWORD_INVALID: "用户密码错误",
-	CODE_USER_NOT_LOGIN:        "用户未登录",
-	CODE_USER_NAME_LEN_INVALID: "用户名长度错误",
-	CODE_USER_EMAIL_INVALID:    "用户邮箱错误",
-	CODE_USER_PHONE_INVALID:    "用户手机号错误",
-	CODE_USER_REPLACE:          "请更换用户名",
-	CODE_USER_AGE_INVALID:      "用户年龄错误",
+	CODE_USER_NAME_EXIST:              "用户名已存在",
+	CODE_USER_NAME_NOT_EXIST:          "用户名不存在",
+	CODE_USER_PASSWORD_INVALID:        "用户密码错误",
+	CODE_PASSWORD_WRONG:               "密码错误",
+	CODE_USER_NOT_LOGIN:               "用户未登录",
+	CODE_USER_NAME_LEN_INVALID:        "用户名长度错误",
+	CODE_USER_EMAIL_INVALID:           "用户邮箱错误",
+	CODE_USER_PHONE_INVALID:           "用户手机号错误",
+	CODE_USER_REPLACE:                 "请更换用户名",
+	CODE_USER_AGE_INVALID:             "用户年龄错误",
+	CODE_USER_AVATAR_NAME_LEN_INVALID: "用户头像名长度错误",
 
 	CODE_CHECKIN_REPEAT:   "重复打卡",
 	CODE_COMMENT_TOO_LONG: "评论最多200字",
