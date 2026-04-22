f8faea524ceddf22f9d20f334f4c7e28d962595c
zzg <zzgzhaoziguang@qq.com>
2026-04-22 23:12:04 +0800

    增加课程详情页

---
+++ internal/app/knowledge_sharing/controller/course_page.go
@@ -0,0 +1,23 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CourseIndexPageHandler(c *gin.Context) {
+	c.HTML(http.StatusOK, "course/index.html", nil)
+}
+
+func CourseDetailPageHandler(c *gin.Context) {
+	c.HTML(http.StatusOK, "course/detail.html", nil)
+}
+
+func AddCoursePageHandler(c *gin.Context) {
+	c.HTML(http.StatusOK, "course/add.html", nil)
+}
+
+func EditCoursePageHandler(c *gin.Context) {
+	c.HTML(http.StatusOK, "course/edit.html", nil)
+}
