f0d85207e73f24ff880a5de5998d97ec77f0a564
zzg <zzgzhaoziguang@qq.com>
2026-05-05 23:21:29 +0800

    增加了添加角色、角色详情页面

--- internal/app/knowledge_sharing/controller/admin_page.go
+++ internal/app/knowledge_sharing/controller/admin_page.go
@@ -17,3 +17,35 @@ func AdminEditPageHandler(c *gin.Context) {
 func AdminRoleEditPageHandler(c *gin.Context) {
 	c.HTML(http.StatusOK, "adminrole/edit.html", nil)
 }
+
+func MenuAddPageHandler(c *gin.Context) {
+	c.HTML(http.StatusOK, "adminmenu/add.html", nil)
+}
+
+func MenuEditPageHandler(c *gin.Context) {
+	c.HTML(http.StatusOK, "adminmenu/edit.html", nil)
+}
+
+func MenuListPageHandler(c *gin.Context) {
+	c.HTML(http.StatusOK, "adminmenu/list.html", nil)
+}
+
+func RoleAddPageHandler(c *gin.Context) {
+	c.HTML(http.StatusOK, "adminrole/add.html", nil)
+}
+
+func RoleListPageHandler(c *gin.Context) {
+	c.HTML(http.StatusOK, "adminrole/list.html", nil)
+}
+
+func RoleDetailPageHandler(c *gin.Context) {
+	c.HTML(http.StatusOK, "adminrole/detail.html", nil)
+}
+
+func AdminUserAddPageHandler(c *gin.Context) {
+	c.HTML(http.StatusOK, "adminuser/add.html", nil)
+}
+
+func AdminUserListPageHandler(c *gin.Context) {
+	c.HTML(http.StatusOK, "adminuser/list.html", nil)
+}
