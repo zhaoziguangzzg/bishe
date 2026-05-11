a9d19877d0cf352cff9ca7d2e1e06c89e514da0d
zzg <zzgzhaoziguang@qq.com>
2026-05-11 12:21:55 +0800

    消费kafka存数据库

--- internal/app/knowledge_sharing/cmd/kafka/notice.go
+++ internal/app/knowledge_sharing/cmd/kafka/notice.go
@@ -9,21 +9,17 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"github.com/IBM/sarama"
 	"go.uber.org/zap"
 )
-
-var noticeChan = make(chan []byte, 10)
-
-//消费kafka，发notice
 
 func main() {
 	err := service.ServiceInit()
 	if err != nil {
 		panic(err)
 	}
-	//main结束之前将日志写到文件
 	defer service.SyncLogger()
 	defer service.Closekafka()
 
@@ -50,85 +46,102 @@ func Notice() {
 			service.Logger.Error("consumerGroup.Close err", zap.Error(err))
 		}
 	}()
-	ctx := context.Background()
 
-	go func() {
-		for {
-			err := consumerGroup.Consume(ctx, []string{topic}, &consumerGroupHandler{})
-			if err != nil {
-				service.Logger.Error("ConsumerGroup.Consume err", zap.Error(err))
-			}
-		}
-	}()
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
-	go func() {
-	notice:
-		for {
-			select {
-			case sig := <-sigChan:
-				service.Logger.Info("KafkaNotice get sig", zap.Any("sig", sig))
-				break notice
-			case msg := <-noticeChan:
-				var noticeMsg model.NoticeMsg
-				err := json.Unmarshal(msg, &noticeMsg)
-				if err != nil {
-					service.Logger.Error("Unmarshal noticeMsg err", zap.Error(err))
-					continue
-				}
-
-				//判断type，组合通知内容
-				userName := noticeMsg.UserName
-				var noticeContent string
-				switch noticeMsg.Type {
-				case model.NOTICE_TYPE_FOLLOW:
-					noticeContent = "又有用户" + userName + "关注啦"
-				case model.NOTICE_TYPE_LIKE:
-					noticeContent = "又有用户" + userName + "点赞啦"
-				case model.NOTICE_TYPE_COMMENT:
-					noticeContent = "又有用户" + userName + "评论啦"
-				case model.NOTICE_TYPE_DISPATCH:
-					noticeContent = "又有用户" + userName + "关注发文啦"
-				case model.NOTICE_TYPE_JOIN:
-					noticeContent = "又有用户" + userName + "加入圈子啦"
-				case model.NOTICE_TYPE_ACCUSATION:
-					noticeContent = "用户" + userName + "举报没有违规"
-				case model.NOTICE_TYPE_ACCUSATIONED:
-					noticeContent = "用户" + userName + "举报有违规"
-				case model.NOTICE_TYPE_FEEDBACK:
-					noticeContent = "用户" + userName + "反馈了"
-
-				default:
-					noticeContent = "又有通知了"
-				}
-				fmt.Println(noticeContent)
-
-				service.Logger.Info("notice msg", zap.Any("noticeMsg", noticeMsg))
-			default:
-			}
-		}
-	}()
-}
-
-type consumerGroupHandler struct{}
-
-// Setup 在每个会话开始前调用
-func (h consumerGroupHandler) Setup(session sarama.ConsumerGroupSession) (err error) {
-	return nil
-}
-
-// Cleanup 在每个会话结束后调用
-func (h consumerGroupHandler) Cleanup(claim sarama.ConsumerGroupSession) (err error) {
-	return nil
-}
-
-// ConsumeClaim 处理分配给该消费者的分区中的消息
-func (h consumerGroupHandler) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) (err error) {
-
-	for message := range claim.Messages() {
-
-		noticeChan <- message.Value
-		session.MarkMessage(message, "")
+	kafkaHandler := &consumerGroupHandler{
+		ready: make(chan bool),
 	}
 
+	go func() {
+		for {
+			//先执行Consume，再执行setup
+			if err := consumerGroup.Consume(ctx, []string{topic}, kafkaHandler); err != nil {
+				service.Logger.Error("ConsumerGroup.Consume err", zap.Error(err))
+			}
+			if ctx.Err() != nil {
+				return
+			}
+			kafkaHandler.ready = make(chan bool)
+		}
+	}()
+
+	<-kafkaHandler.ready
+
+	sig := <-sigChan
+	service.Logger.Info("KafkaNotice get sig", zap.Any("sig", sig))
+	cancel()
+}
+
+func processOneNoticeStr(msg []byte) (err error) {
+	var noticeMsg model.NoticeMsg
+	err = json.Unmarshal(msg, &noticeMsg)
+	if err != nil {
+		service.Logger.Error("Unmarshal noticeMsg err", zap.Error(err))
+		return
+	}
+
+	userName := noticeMsg.UserName
+	var noticeContent string
+	switch noticeMsg.Type {
+	case model.NOTICE_TYPE_FOLLOW:
+		noticeContent = "又有用户" + userName + "关注啦"
+	case model.NOTICE_TYPE_LIKE:
+		noticeContent = "又有用户" + userName + "点赞啦"
+	case model.NOTICE_TYPE_COMMENT:
+		noticeContent = "又有用户" + userName + "评论啦"
+	case model.NOTICE_TYPE_DISPATCH:
+		noticeContent = "又有用户" + userName + "关注发文啦"
+	case model.NOTICE_TYPE_JOIN:
+		noticeContent = "又有用户" + userName + "加入圈子啦"
+	case model.NOTICE_TYPE_ACCUSATION:
+		noticeContent = "用户" + userName + "举报没有违规"
+	case model.NOTICE_TYPE_ACCUSATIONED:
+		noticeContent = "用户" + userName + "举报有违规"
+	case model.NOTICE_TYPE_FEEDBACK:
+		noticeContent = "用户" + userName + "反馈了"
+	default:
+		noticeContent = "又有通知了"
+	}
+
+	fmt.Println(noticeContent)
+	nowTime := time.Now()
+
+	err = service.UserAddNotice(noticeMsg.Uid, noticeContent, noticeMsg.Type, nowTime)
+	if err != nil {
+		service.Logger.Error("UserAddNotice err", zap.Error(err))
+		return
+	}
+
+	service.Logger.Info("notice msg", zap.Any("noticeMsg", noticeMsg))
+	return
+}
+
+type consumerGroupHandler struct {
+	ready chan bool
+}
+
+func (h *consumerGroupHandler) Setup(session sarama.ConsumerGroupSession) error {
+	close(h.ready)
 	return nil
+}
+
+func (h *consumerGroupHandler) Cleanup(session sarama.ConsumerGroupSession) error {
+	return nil
+}
+
+func (h *consumerGroupHandler) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
+	for {
+		select {
+		case message, ok := <-claim.Messages():
+			if !ok {
+				return nil
+			}
+			processOneNoticeStr(message.Value)
+			session.MarkMessage(message, "")
+		case <-session.Context().Done():
+			return nil
+		}
+	}
 }
