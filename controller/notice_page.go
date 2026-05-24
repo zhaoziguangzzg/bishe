4e685d2d944d842a75f91f0e615dc03dd1b53025
zzg <zhaoziguangzzg@qq.com>
2026-05-24 09:56:55 +0800

    notice通知使用layout

--- controller/notice_page.go
+++ controller/notice_page.go
@@ -1,16 +1,13 @@
 package controller
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
-// OrdersIndexPageHandler
 func NoticeIndexPageHandler(c *gin.Context) {
-	c.HTML(http.StatusOK, "notice/index.html", nil)
+	RenderIndexPage(c, "notice/index.html", nil)
 }
 
 func NoticeDetailPageHandler(c *gin.Context) {
-	c.HTML(http.StatusOK, "notice/detail.html", nil)
+	RenderIndexPage(c, "notice/detail.html", nil)
 }
