1a75954073c91611a611d34f39bd6e269c8d82df
zzg <zhaoziguangzzg@qq.com>
2026-05-24 09:49:28 +0800

    announce公告使用layout

--- controller/announce_page.go
+++ controller/announce_page.go
@@ -1,15 +1,13 @@
 package controller
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
 func AnnounceListPageHandler(c *gin.Context) {
-	c.HTML(http.StatusOK, "announce/list.html", nil)
+	RenderIndexPage(c, "announce/list.html", nil)
 }
 
 func AnnounceDetailPageHandler(c *gin.Context) {
-	c.HTML(http.StatusOK, "announce/detail.html", nil)
+	RenderIndexPage(c, "announce/detail.html", nil)
 }
